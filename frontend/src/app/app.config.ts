cd3b8357feb3d7595fab7a2290eae7b989dc6231
noobdevsam <mdshoykotislam05@gmail.com>
2026-08-30 02:54:44 +0600

    Update app configuration to use Zone Change Detection and HttpClient

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,8 +1,9 @@
-import {ApplicationConfig, provideBrowserGlobalErrorListeners} from '@angular/core';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideHttpClient} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-
+    provideZoneChangeDetection({eventCoalescing: true}),
+    provideHttpClient()
   ]
 };
